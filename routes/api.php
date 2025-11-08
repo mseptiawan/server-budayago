96929a6373eb328236c381dd0c775a0a8e1b3d46
mseptiawan <mseptiawan017@gmail.com>
2025-11-08 22:15:18 +0700

    controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CultureController;
 
@@ -8,3 +9,9 @@ Route::post('/cultures', [CultureController::class, 'store']);
 Route::get('/cultures/{id}', [CultureController::class, 'show']);
 Route::put('/cultures/{id}', [CultureController::class, 'update']);
 Route::delete('/cultures/{id}', [CultureController::class, 'destroy']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
